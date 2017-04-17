d2945cd6663fbcc15d8c4269a492cdeb0c6990ff
Juan DeBiedma <juanthefatty@gmail.com>
2017-04-17 18:01:22 -0400

    changes

--- bamazonManager.js
+++ bamazonManager.js
@@ -60,6 +60,55 @@ function startManager() {
                     });
 
                 }
+
+                else if (manager.option === "Add to Inventory") {
+
+                    connection.query("SELECT * FROM products", function(err, res) {
+
+                        if (err) throw err;
+
+                        console.table(res);
+
+
+                        prompt.start();
+                                      // 
+                        prompt.get([
+
+                        {
+                          name: 'id',
+                          description:'Please enter the ID of the item you would like to add inventory for',
+                          message: 'That is not a valid ID.',
+                          type: 'integer',
+                          required: true
+                        },
+
+                        {
+                          name: 'amount',
+                          description:'Please enter the amount of units you are adding to this inventory',
+                          message: 'That is not a valid number.',
+                          type: 'integer',
+                          required: true
+                        },
+
+                      
+                        ], function (err, result) {
+
+                                for (var i = 0; i < res.length; i++) {
+
+                                    if (result.id === res[i].item_id) {
+
+                                        console.log("You have added "+ result.amount + " more unit(s) of " 
+                                            + res[i].product_name + " to the inventory.");
+
+                                        
+
+                                    }
+                                }    
+                        });
+
+                    });
+
+                }
   
           });
 
